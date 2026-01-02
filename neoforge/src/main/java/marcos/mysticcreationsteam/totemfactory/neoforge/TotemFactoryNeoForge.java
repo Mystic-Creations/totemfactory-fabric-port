aa86f4bd6f40a898e0c38c14102037ee7dc8271f
JustMili <rodx160@gmail.com>
2026-01-02 23:08:55 +0100

    still trying to adapt NeoForge even tho gradle is broken (BluSpring wtf did you do in Create Fucked Up that it's THAT FUCKED UP?)

--- neoforge/src/main/java/marcos/mysticcreationsteam/totemfactory/neoforge/TotemFactoryNeoForge.java
+++ neoforge/src/main/java/marcos/mysticcreationsteam/totemfactory/neoforge/TotemFactoryNeoForge.java
@@ -2,12 +2,10 @@ package marcos.mysticcreationsteam.totemfactory.neoforge;
 
 import net.neoforged.fml.common.Mod;
 
-import marcos.mysticcreationsteam.totemfactory.ExampleMod;
+import marcos.mysticcreationsteam.totemfactory.TotemFactory;
 
-@Mod(ExampleMod.MOD_ID)
-public final class ExampleModNeoForge {
-    public ExampleModNeoForge() {
-        // Run our common setup.
-        ExampleMod.init();
+@Mod(TotemFactory.MODID)
+public final class TotemFactoryNeoForge {
+    public TotemFactoryNeoForge() {
     }
 }
