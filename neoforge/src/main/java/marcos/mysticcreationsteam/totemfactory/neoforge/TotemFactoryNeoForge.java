dee6db6b5afee098e42ecf6acace0a50f8ea858f
JustMili <rodx160@gmail.com>
2026-01-02 21:55:37 +0100

    stuff

---
+++ neoforge/src/main/java/marcos/mysticcreationsteam/totemfactory/neoforge/TotemFactoryNeoForge.java
@@ -0,0 +1,13 @@
+package marcos.mysticcreationsteam.totemfactory.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import marcos.mysticcreationsteam.totemfactory.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
