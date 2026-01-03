dee9a0a62654c4da0435d42b9a352f9c3f99bdc1
JustMili <rodx160@gmail.com>
2026-01-03 21:22:21 +0100

    Update file getter tool

--- .builtJars/getFiles.bat
+++ .builtJars/getFiles.bat
@@ -1,9 +1,7 @@
 @echo off
 echo Moving files...
 move ..\fabric\build\libs\*.jar . >nul
-move ..\forge\build\libs\*.jar . >nul
-::left this for when we'll do 1.21.1
-::move ..\neoforge\build\libs\*.jar . >nul
+move ..\neoforge\build\libs\*.jar . >nul
 
 timeout /t 3 /nobreak >nul
 echo Deleting shadow jars...
--- .builtJars/getFiles.sh
+++ .builtJars/getFiles.sh
@@ -2,9 +2,7 @@
 
 echo "Moving files..."
 mv ../fabric/build/libs/*.jar . 2>/dev/null
-mv ../forge/build/libs/*.jar . 2>/dev/null
-# left this for when we'll do 1.21.1
-# mv ../neoforge/build/libs/*.jar . 2>/dev/null
+mv ../neoforge/build/libs/*.jar . 2>/dev/null
 
 sleep 3
 
