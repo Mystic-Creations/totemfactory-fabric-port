d5b9e17306940496486754245a4460a6ba13648e
JustMili <rodx160@gmail.com>
2026-01-04 16:45:57 +0100

    Updated jar getting tool 1.21.1

--- .builtJars/getFiles.bat
+++ .builtJars/getFiles.bat
@@ -2,9 +2,7 @@
 echo Moving files...
 move ..\fabric\build\libs\*.jar . >nul
 move ..\neoforge\build\libs\*.jar . >nul
-
-timeout /t 3 /nobreak >nul
 echo Deleting shadow jars...
 del /q *dev-shadow.jar >nul
-
-timeout 3 >nul
+echo Done.
+timeout 1 >nul
--- .builtJars/getFiles.sh
+++ .builtJars/getFiles.sh
@@ -3,10 +3,7 @@
 echo "Moving files..."
 mv ../fabric/build/libs/*.jar . 2>/dev/null
 mv ../neoforge/build/libs/*.jar . 2>/dev/null
-
-sleep 3
-
 echo "Deleting shadow jars..."
 rm -f *dev-shadow.jar 2>/dev/null
-
-sleep 3
+echo "Done."
+sleep 1
