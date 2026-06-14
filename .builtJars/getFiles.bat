7ee8ee763117e2797da960e347db348940dcfc65
JustMili <rodx160@gmail.com>
2026-06-15 00:49:59 +0200

    Update buildAndGet script

--- .builtJars/getFiles.bat
+++ .builtJars/getFiles.bat
@@ -1,8 +1,20 @@
 @echo off
+
+cd /D %~dp0
+del /q *.jar >nul
+
+echo Building...
+cd /D ../
+powershell -c "./gradlew :fabric:build"
+powershell -c "./gradlew :forge:build"
+
+cd /D %~dp0
 echo Moving files...
 move ..\fabric\build\libs\*.jar . >nul
 move ..\forge\build\libs\*.jar . >nul
+
 echo Deleting shadow jars...
 del /q *dev-shadow.jar >nul
+
 echo Done.
-timeout 1 >nul
+pause >nul
